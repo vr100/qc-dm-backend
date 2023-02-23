6c6dd842f75b9e00207bda5e5f4248a9283ad540
vr100 <vr100@users.noreply.github.com>
2023-02-24 00:39:21 +0530

    adding the basic setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+''' Qiskit aakash density matrix backend '''
+
+import os
+
+from setuptools import setup
+
+requirements = [
+    "qiskit-terra~=0.23.0"
+]
+
+readme_path = os.path.join(os.path.abspath(
+    os.path.dirname(__file__)), 'README.md')
+with open(readme_path) as readme_file:
+    long_desc = readme_file.read()
+
+
+setup(
+    name="qc-dm-backend",
+    version="0.1",
+    description="Qiskit aakash backend using density matrix",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url="https://github.com/vr100/qc-dm-backend",
+    author="vr100",
+    author_email="vr100@@",
+    license="Apache 2.0",
+    keywords="qiskit aakash backend",
+    packages=['qiskit.providers.aakash'],
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires=">=3.9"
+)
